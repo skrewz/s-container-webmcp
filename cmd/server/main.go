6f41aa3d0582f267ab21ff5a9d94b98c59a5d61e
Anders Breindahl <skrewz@skrewz.net>
2026-03-19 12:44:06 +1100

    📄 Remove incorrect copyright notice
    (Left in here by an AI coding model.)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,3 @@
-// Copyright 2025 The Go MCP SDK Authors. All rights reserved.
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
